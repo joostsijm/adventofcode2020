e865d6df41d72b5bff857f899ade4b4e800cb612
JoostSijm <joostsijm@gmail.com>
2020-12-04 14:47:25 +0100

    Finish day 3

--- day3/day3.py
+++ day3/day3.py
@@ -13,6 +13,31 @@ def main():
             width += 1
         height += 1
 
+    trees = [
+        calculate_trees(slope, 1, 1),
+        calculate_trees(slope, 1, 3),
+        calculate_trees(slope, 1, 5),
+        calculate_trees(slope, 1, 7),
+        calculate_trees(slope, 2, 1),
+    ]
+    multiplied_trees = 1
+    for tree in trees:
+        multiplied_trees *= tree
+    print(multiplied_trees)
+
+def calculate_trees(slope, down, right):
+    """Calculate number of trees"""
+    trees = 0
+    for row in range(0, len(slope), down):
+        position = get_position(slope, row, row * right / down)
+        if position == '#':
+            trees += 1
+    return trees
+
+def get_position(slope, height, width):
+    """Get position from slope"""
+    return slope[height][width % len(slope[0])]
+
 def read():
     """Read file"""
     lines = []
